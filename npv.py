7421d7d3de440c573f89dd9a6481e72fb81984bb
jbrust91 <jordanrust@gmail.com>
2017-07-13 02:21:45 -0600

    Update npv.py
    Built a loop into the code, so that a time period over 99 would loop back to the question.

--- npv.py
+++ npv.py
@@ -9,5 +9,14 @@ if t<=99:
     npv=(pv*(1+r)**t)
     nnpv=(format_currency(npv,'USD',locale='en_US'))
     print('In',t,'year(s), you will have',nnpv)
-
+while t>99:
+    print('Please try a different number')
+    t=int(input('What is your Number of Periods (1-99):'))
+    if t<=99:
+        npv=(pv*(1+r)**t)
+        nnpv=(format_currency(npv,'USD',locale='en_US'))
+        print('In',t,'year(s), your net present value will be',nnpv)
+        break
+    if t>99:
+        continue
 
